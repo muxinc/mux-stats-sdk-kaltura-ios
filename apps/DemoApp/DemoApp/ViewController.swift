88f8393d3cf6db067727248f85911b3259bc349d
StephanieZuniga <40036667+StephanieZuniga@users.noreply.github.com>
2021-09-28 16:46:46 -0600

    [ch3126] Setup example project (#2)
    * [ch3126] Setup example project with play/pause/replay button and slider for seeking with Kaltura Playkit player.

--- apps/DemoApp/DemoApp/ViewController.swift
+++ apps/DemoApp/DemoApp/ViewController.swift
@@ -7,14 +7,207 @@
 //
 
 import UIKit
+import PlayKit
 
 class ViewController: UIViewController {
+    var kalturaPlayer: Player?
+    let kalturaPlayerContainer = PlayerView()
+    let playButton = UIButton()
+    let playheadSlider = UISlider()
+    let positionLabel = UILabel()
+    let durationLabel = UILabel()
+    
+    private var playerState: PlayerState = .idle {
+        didSet {
+            // Update player button icon depending on the state
+            switch playerState {
+            case .idle:
+                self.playButton.setImage(UIImage(systemName: "play"), for: .normal)
+            case .playing:
+                self.playButton.setImage(UIImage(systemName: "pause"), for: .normal)
+            case .paused:
+                self.playButton.setImage(UIImage(systemName: "play"), for: .normal)
+            case .ended:
+                self.playButton.setImage(UIImage(systemName: "arrow.clockwise"), for: .normal)
+            }
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.setupLayout()
+        
+        // Load PlayKit player
+        self.kalturaPlayer = PlayKitManager.shared.loadPlayer(pluginConfig: nil)
+        self.setupKalturaPlayer()
     }
+    
+    func setupKalturaPlayer() {
+        // Set PlayerView as the container for PlayKit Player variable
+        self.kalturaPlayer?.view = self.kalturaPlayerContainer
+        self.loadMediaKalturaPlayer()
+        
+        
+        // Handle PlayKit events
+        self.playerState = .idle
+        let events = [
+            PlayerEvent.pause,
+            PlayerEvent.playing,
+            PlayerEvent.ended
+        ]
+        
+        // Update player state depending on the Playkit events
+        self.kalturaPlayer?.addObserver(self, events: events) { [weak self] (event) in
+            guard let self = self else { return }
 
-
+            switch event {
+            case is PlayerEvent.Playing:
+                self.playerState = .playing
+            case is PlayerEvent.Pause:
+                self.playerState = .paused
+            case is PlayerEvent.Ended:
+                self.playerState = .ended
+            default:
+                break
+            }
+        }
+        
+        // Checks media progress to update the player slider and the current position label
+        _ = self.kalturaPlayer?.addPeriodicObserver(
+            interval: 0.2,
+            observeOn: DispatchQueue.main,
+            using: { [weak self] currentPosition in
+                self?.playheadSlider.value = Float(currentPosition)
+                self?.positionLabel.text = currentPosition.formattedTimeDisplay
+            }
+        )
+        
+        // Observe PlayKit event durationChanged to update the maximum duration of the slider and duration label
+        self.kalturaPlayer?.addObserver(
+            self,
+            events: [PlayerEvent.durationChanged],
+            block: { [weak self] event in
+                guard
+                    let self = self,
+                    let durationEvent = event as? PlayerEvent.DurationChanged,
+                    let duration = durationEvent.duration as? TimeInterval
+                else {
+                    return
+                }
+                
+                self.playheadSlider.maximumValue = Float(duration)
+                self.durationLabel.text = duration.formattedTimeDisplay
+            }
+        )
+    }
+    
+    func loadMediaKalturaPlayer() {
+        let contentURL = "https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8"
+        let entryId = "sintel"
+        
+        // Create PlayKit media source
+        let source = PKMediaSource(entryId, contentUrl: URL(string: contentURL), drmData: nil, mediaFormat: .hls)
+        
+        // Setup PlayKit media entry
+        let mediaEntry = PKMediaEntry(entryId, sources: [source])
+        
+        // Create PlayKit media config
+        let mediaConfig = MediaConfig(mediaEntry: mediaEntry)
+        
+        // Prepare PlayKit player
+        self.kalturaPlayer!.prepare(mediaConfig)
+    }
+    
+    @objc func playButtonPressed() {
+        guard let player = self.kalturaPlayer else {
+            return
+        }
+        
+        // Handle PlayKit events
+        switch playerState {
+        case .playing:
+            player.pause()
+        case .idle:
+            player.play()
+        case .paused:
+            player.play()
+        case .ended:
+            player.seek(to: 0)
+            player.play()
+        }
+    }
+    
+    @objc func playheadValueChanged() {
+        guard let player = self.kalturaPlayer else {
+            return
+        }
+        
+        if self.playerState == .ended && self.playheadSlider.value < self.playheadSlider.maximumValue {
+            self.playerState = .paused
+        }
+        
+        player.currentTime = TimeInterval(self.playheadSlider.value)
+    }
 }
 
+extension ViewController {
+    enum PlayerState {
+        case idle
+        case playing
+        case paused
+        case ended
+    }
+}
+
+extension ViewController {
+    func setupLayout() {
+        self.view.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(self.kalturaPlayerContainer)
+        
+        // Constraint PlayKit player container to safe area layout guide
+        self.kalturaPlayerContainer.translatesAutoresizingMaskIntoConstraints = false
+        let guide = self.view.safeAreaLayoutGuide
+        NSLayoutConstraint.activate([
+            self.kalturaPlayerContainer.topAnchor.constraint(equalTo: guide.topAnchor),
+            self.kalturaPlayerContainer.bottomAnchor.constraint(equalTo: guide.bottomAnchor),
+            self.kalturaPlayerContainer.leadingAnchor.constraint(equalTo: guide.leadingAnchor),
+            self.kalturaPlayerContainer.trailingAnchor.constraint(equalTo: guide.trailingAnchor)
+        ])
+        
+        let actionsContainer = UIStackView()
+        actionsContainer.axis = .horizontal
+        actionsContainer.spacing = 6.0
+        actionsContainer.isLayoutMarginsRelativeArrangement = true
+        actionsContainer.layoutMargins = UIEdgeInsets(top: 0, left: 8.0, bottom: 0, right: 8.0)
+        actionsContainer.translatesAutoresizingMaskIntoConstraints = false
+        self.kalturaPlayerContainer.addSubview(actionsContainer)
+        NSLayoutConstraint.activate([
+            actionsContainer.bottomAnchor.constraint(equalTo: self.kalturaPlayerContainer.bottomAnchor),
+            actionsContainer.heightAnchor.constraint(equalToConstant: 64.0),
+            actionsContainer.leadingAnchor.constraint(equalTo: self.kalturaPlayerContainer.leadingAnchor),
+            actionsContainer.trailingAnchor.constraint(equalTo: self.kalturaPlayerContainer.trailingAnchor)
+        ])
+        
+        // Add play/pause button
+        self.playButton.addTarget(self, action: #selector(self.playButtonPressed), for: .touchUpInside)
+        self.playButton.contentEdgeInsets = UIEdgeInsets(top: 20, left: 4, bottom: 20, right: 4)
+        self.playButton.contentHorizontalAlignment = .fill
+        self.playButton.contentVerticalAlignment = .fill
+        actionsContainer.addArrangedSubview(self.playButton)
+        NSLayoutConstraint.activate([
+            self.playButton.widthAnchor.constraint(equalToConstant: 28.0)
+        ])
+        
+        self.positionLabel.textColor = .lightText
+        self.positionLabel.text = TimeInterval.zero.formattedTimeDisplay
+        actionsContainer.addArrangedSubview(self.positionLabel)
+        
+        self.playheadSlider.addTarget(self, action: #selector(self.playheadValueChanged), for: .valueChanged)
+        actionsContainer.addArrangedSubview(self.playheadSlider)
+        
+        self.durationLabel.textColor = .lightText
+        self.durationLabel.text = TimeInterval.zero.formattedTimeDisplay
+        actionsContainer.addArrangedSubview(self.durationLabel)
+    }
+}
