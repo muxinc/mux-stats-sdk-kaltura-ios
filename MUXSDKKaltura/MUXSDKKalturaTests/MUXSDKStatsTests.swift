28a223fecf1b1be8bbcc1db027a716262f2717b6
StephanieZuniga <40036667+StephanieZuniga@users.noreply.github.com>
2022-02-09 12:20:36 -0500

    Release v1.1.0
    * Test: Unit test for destroy player
    * Feature: Support for google ima sdk listener implementation
    
    Co-authored-by: Audra Rodriguez <arodriguez@konradgroup.com>
    Co-authored-by: Stephanie Zuniga <szuniga@konradgroup.com>

--- MUXSDKKaltura/MUXSDKKalturaTests/MUXSDKStatsTests.swift
+++ MUXSDKKaltura/MUXSDKKalturaTests/MUXSDKStatsTests.swift
@@ -172,4 +172,133 @@ class MUXSDKStatsTests: XCTestCase {
         )
         self.assertDispatchedCustomerViewerDataEventsAtIndex(index: 0, expectedCustomerViewerData: expectedViewerData)
     }
+    
+    func testDestroyPlayer() {
+        guard
+            let customerData = MockedData.customerData
+        else {
+            XCTFail("Customer data not found")
+            return
+        }
+       
+        // Start monitor player to emit events
+        MUXSDKStats.monitorPlayer(player: MockedData.player, playerName: MockedData.playerName, customerData: customerData)
+       
+        // Remove observers on the player
+        MUXSDKStats.destroyPlayer(name: MockedData.playerName)
+
+        let expectedEvents = [
+            MUXSDKPlaybackEventViewInitEventType,
+            MUXSDKDataEventType,
+            MUXSDKPlaybackEventPlayerReadyEventType,
+            MUXSDKPlaybackEventViewEndEventType
+        ]
+
+        // Assert expected events
+        self.assertDispatchedEventTypesMatch(
+            expectedEventTypes: expectedEvents,
+            for: MockedData.playerName
+        )
+
+        // Clean player instance after test
+        MUXSDKStats.destroyPlayer(name: MockedData.playerName)
+    }
+       
+    func testClearsCustomerMetadataOnDestroy() {
+        let videoData = MUXSDKCustomerVideoData()
+        videoData.videoTitle = "Video Title"
+        videoData.videoId = "videoId"
+        videoData.videoSeries = "series"
+
+        let customData = MUXSDKCustomData()
+        customData.customData1 = "Custom Data 1"
+        customData.customData2 = "Custom Data 2"
+
+        let updatedVideoData = MUXSDKCustomerVideoData()
+        updatedVideoData.videoTitle = "Video Title Version Clears Customer Metadata On Destroy"
+        updatedVideoData.videoId = "videoId Version Clears Customer Metadata On Destroy"
+        updatedVideoData.videoSeries = "series Version Clears Customer Metadata On Destroy"
+
+        let baseCustomerData = MockedData.buildCustomerData(videoData: videoData, customData: customData)
+        let newCustomerData = MockedData.buildCustomerData(videoData: updatedVideoData)
+        guard
+            let customerData = baseCustomerData,
+            let customerDataUpdated = newCustomerData
+        else {
+            XCTFail("Customer data not found")
+            return
+        }
+
+        // Start monitor player to emit events
+        MUXSDKStats.monitorPlayer(player: MockedData.player, playerName: MockedData.playerName, customerData: customerData)
+
+        var expectedEvents = [
+            MUXSDKPlaybackEventViewInitEventType,
+            MUXSDKDataEventType,
+            MUXSDKPlaybackEventPlayerReadyEventType
+        ]
+        let expectedCustomerVideoData: [String : Any] = [
+            "vtt" : "Video Title",
+            "vid" : "videoId",
+            "vsr" : "series"
+        ]
+
+        let expectedCustomData: [String : Any] = [
+            "c1" : "Custom Data 1",
+            "c2" : "Custom Data 2"
+        ]
+
+        // Assert expected events
+        self.assertDispatchedEventTypesMatch(
+            expectedEventTypes: expectedEvents,
+            for: MockedData.playerName
+        )
+
+        // Assert expected data
+        self.assertDispatchedCustomerDataEventsMatch(
+            expectedCustomerVideoData: expectedCustomerVideoData,
+            expectedCustomData: expectedCustomData,
+            at: 1
+        )
+
+        // Remove observers on the player
+        MUXSDKStats.destroyPlayer(name: MockedData.playerName)
+
+        // Update customer data
+        MUXSDKStats.setCustomerDataForPlayer(name: MockedData.playerName, customerData: customerDataUpdated)
+
+        // Start monitor player to emit events
+        MUXSDKStats.monitorPlayer(player: MockedData.player, playerName: MockedData.playerName, customerData: customerDataUpdated)
+
+        expectedEvents = [
+            MUXSDKPlaybackEventViewInitEventType,
+            MUXSDKDataEventType,
+            MUXSDKPlaybackEventPlayerReadyEventType,
+            MUXSDKPlaybackEventViewEndEventType,
+            MUXSDKDataEventType,
+            MUXSDKPlaybackEventViewInitEventType,
+            MUXSDKDataEventType,
+            MUXSDKPlaybackEventPlayerReadyEventType
+        ]
+        let expectedCustomerUpdatedVideoData = [
+            "vtt" : "Video Title Version Clears Customer Metadata On Destroy",
+            "vid" : "videoId Version Clears Customer Metadata On Destroy",
+            "vsr" : "series Version Clears Customer Metadata On Destroy"
+        ]
+
+        // Assert expected events
+        self.assertDispatchedEventTypesMatch(
+            expectedEventTypes: expectedEvents,
+            for: MockedData.playerName
+        )
+
+        // Assert expected updated data
+        self.assertDispatchedCustomerDataEventsMatch(
+            expectedCustomerVideoData: expectedCustomerUpdatedVideoData,
+            at: 4
+        )
+
+        // Clean player instance after test
+        MUXSDKStats.destroyPlayer(name: MockedData.playerName)
+    }
 }
