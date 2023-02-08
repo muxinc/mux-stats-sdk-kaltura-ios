3346b535697decbd0ce47a4a2a258bbe62ef4984
Emily Dixon <edixon@mux.com>
2023-02-07 17:14:25 -0800

    Wait I fixed cocoapods

--- .buildkite/unit-tests.sh
+++ .buildkite/unit-tests.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 set -euo pipefail
 
-#cd MUXSDKKaltura
-#pod repo update
-#pod deintegrate && pod install
-#cd ..
+cd MUXSDKKaltura
+pod repo update
+pod deintegrate && pod install
+cd ..
 PROJECT=MUXSDKKaltura/MUXSDKKaltura.xcworkspace
-
-echo " PRINTING WORKING DIR ------------"
-pwd
 
 xcodebuild clean test \
   -workspace $PROJECT \
