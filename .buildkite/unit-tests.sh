35e98576239823ef66c2aad86d7b64feec53c71b
Emily Dixon <edixon@mux.com>
2023-02-07 17:03:42 -0800

    Use a newer simulator

--- .buildkite/unit-tests.sh
+++ .buildkite/unit-tests.sh
@@ -2,12 +2,12 @@
 set -euo pipefail
 
 cd MUXSDKKaltura
-pod repo update
-pod deintegrate && pod install
+#pod repo update
+#pod deintegrate && pod install
 cd ..
 PROJECT=MUXSDKKaltura/MUXSDKKaltura.xcworkspace
 
 xcodebuild clean test \
   -workspace $PROJECT \
   -scheme MUXSDKKaltura \
-  -destination 'platform=iOS Simulator,name=iPhone 11,OS=14.1' \
+  -destination 'platform=iOS Simulator,name=iPhone 14,OS=16.2' \
