310b4475ec54d406153cc71730626c58df625c49
AJ Lauer Barinov <abarinov@mux.com>
2023-09-15 11:46:20 -0700

    remove iOS 10 version check

--- Sources/MUXSDKStatsKaltura/MUXSDKPlayerBinding.swift
+++ Sources/MUXSDKStatsKaltura/MUXSDKPlayerBinding.swift
@@ -419,7 +419,6 @@ public class MUXSDKPlayerBinding: NSObject {
     // FIXME: test if needed and if it works as expected
     func handleRebufferingInAirplayMode() {
         guard
-            #available(iOS 10.0, tvOS 10.0, *),
             let playerLayer = player?.view?.layer as? AVPlayerLayer,
             let player = playerLayer.player,
             player.timeControlStatus != .playing,
