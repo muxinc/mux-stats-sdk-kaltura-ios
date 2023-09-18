564348e49608ac60fdd3d57a6c2a453252679ed1
AJ Lauer Barinov <abarinov@mux.com>
2023-09-18 14:18:45 -0700

    ci: correct iOS version

--- scripts/run-unit-tests-using-xcode-rc.sh
+++ scripts/run-unit-tests-using-xcode-rc.sh
@@ -9,16 +9,16 @@ echo "▸ Available Xcode SDKs"
 
 xcodebuild -showsdks
 
-echo "▸ Testing SDK on iOS 17"
+echo "▸ Testing SDK on iOS 17.0"
 
 xcodebuild clean test \
   -scheme "mux-stats-sdk-kaltura-ios" \
   -sdk iphonesimulator17.0 \
-  -destination 'platform=iOS Simulator,name=iPhone 15,OS=17' | xcbeautify
+  -destination 'platform=iOS Simulator,name=iPhone 15,OS=17.0' | xcbeautify
 
-echo "▸ Testing SDK on tvOS 17"
+echo "▸ Testing SDK on tvOS 17.0"
 
 xcodebuild clean test \
   -scheme "mux-stats-sdk-kaltura-ios" \
   -sdk appletvsimulator17.0 \
-  -destination 'platform=tvOS Simulator,name=Apple TV,OS=17' | xcbeautify
+  -destination 'platform=tvOS Simulator,name=Apple TV,OS=17.0' | xcbeautify
