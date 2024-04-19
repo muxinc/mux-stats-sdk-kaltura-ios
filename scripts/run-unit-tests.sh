d18c4d8d68208245abf25c4371226fd1280b7bb3
AJ Lauer Barinov <abarinov@mux.com>
2024-04-19 16:42:12 -0700

    echo the right thing

--- scripts/run-unit-tests.sh
+++ scripts/run-unit-tests.sh
@@ -12,7 +12,7 @@ echo "▸ Available Xcode SDKs"
 
 xcodebuild -showsdks
 
-echo "▸ xcodebuild clean test -scheme "mux-stats-sdk-kaltura-ios" -sdk iphonesimulator16.4 -destination 'name=iPhone 14,OS=16.4'"
+echo "▸ xcodebuild clean test -scheme "mux-stats-sdk-kaltura-ios" -sdk iphonesimulator17.4 -destination 'name=iPhone 15,OS=17.4'"
 
 xcodebuild clean test \
   -scheme "mux-stats-sdk-kaltura-ios" \
